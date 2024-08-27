27043148d3847cc34d0d48a92c25f8b0a91a5072
mominul7862 <mominulislam7862@gmail.com>
2024-08-27 17:47:13 +0600

    3rd commit'Our Services'

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,43 +2,86 @@ import img1 from '../../../assets/images/banner/1.jpg';
 import img2 from '../../../assets/images/banner/2.jpg';
 import img3 from '../../../assets/images/banner/3.jpg';
 import img4 from '../../../assets/images/banner/4.jpg';
-import img5 from '../../../assets/images/banner/5.jpg';
-import img6 from '../../../assets/images/banner/6.jpg';
 
-const Banner = () => {
+
+const Banner = () => { 
     return (
-        <div className="carousel box-content h-80 ">
+        <div className="carousel box-content h-[600px] ">
             <div id="slide1" className="carousel-item relative w-full">
                 <img
                     src={img1}
-                    className="w-full" />
+                    className="w-full rounded-xl" />
+                <div className="absolute rounded-xl h-full flex items-center  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21, 0)] ">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                        <button className="btn btn-active btn-primary">Discover more</button>
+                        <button className="btn btn-outline btn-secondary">Latest projects</button>
+
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                     <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img
+            <img
                     src={img2}
-                    className="w-full" />
+                    className="w-full rounded-xl" />
+                <div className="absolute rounded-xl h-full flex items-center  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21, 0)] ">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                        <button className="btn btn-active btn-primary">Discover more</button>
+                        <button className="btn btn-outline btn-secondary">Latest projects</button>
+
+                        </div>
+                    </div>
+                </div>
                <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img
+            <img
                     src={img3}
-                    className="w-full" />
+                    className="w-full rounded-xl" />
+                <div className="absolute rounded-xl h-full flex items-center  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21, 0)] ">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                        <button className="btn btn-active btn-primary">Discover more</button>
+                        <button className="btn btn-outline btn-secondary">Latest projects</button>
+
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
-                <img
+            <img
                     src={img4}
-                    className="w-full" />
+                    className="w-full rounded-xl" />
+                <div className="absolute rounded-xl h-full flex items-center  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21, 0)] ">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                        <button className="btn btn-active btn-primary">Discover more</button>
+                        <button className="btn btn-outline btn-secondary">Latest projects</button>
+
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import About from '../About/About';
+import Services from '../services/Services';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <h2>This is Home</h2>
+            <About></About>
+            <Services></Services>
         </div>
     );
 };
